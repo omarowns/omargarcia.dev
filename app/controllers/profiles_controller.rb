3234207a0036c0acb1f81495e0289534ee91aee1
Omar Garcia <isc.omargarcia@gmail.com>
2021-04-08 13:10:15 -0500

    Adding admin profiles styles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,4 @@
-class ProfilesController < ApplicationController
+class ProfilesController < AdminController
   before_action :set_profile, only: [:show, :edit, :update, :destroy]
 
   # GET /profiles
