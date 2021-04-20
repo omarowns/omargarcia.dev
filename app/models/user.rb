4183f54696ff6e61746737991adc55e3582f5ed1
Omar Garcia <isc.omargarcia@gmail.com>
2021-04-20 16:19:27 -0500

    disable registrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,8 +19,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable,
-         :validatable, :omniauthable, omniauth_providers: %i(github spotify twitter)
+  devise :database_authenticatable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i(github spotify twitter)
 
   has_many :authenticables, dependent: :destroy
 end
