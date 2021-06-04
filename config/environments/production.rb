1a9103c7926689b9e2710c2247a33b9a47179b06
Omar Garcia <isc.omargarcia@gmail.com>
2021-06-04 15:40:36 -0500

    Fix a image upload issue within activetext

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,8 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default_url_options = { host: 'omargarcia.mx' }
+  config.action_controller.default_url_options = { host: 'omargarcia.mx' }
+  routes.default_url_options = { host: 'omargarcia.mx' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
