88b50df8b340adcb5796f3e08a6bd538390caffb
Omar Garcia <isc.omargarcia@gmail.com>
2021-06-04 22:20:39 -0500

    remove prefix from sidekiq queue on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,6 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter     = :sidekiq
-  config.active_job.queue_name_prefix = "omargarcia_dev_production"
 
   config.action_mailer.perform_caching = false
 
